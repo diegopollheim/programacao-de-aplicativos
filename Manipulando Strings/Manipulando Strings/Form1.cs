71a21abc641c1b3de4af29dcc487de788927d484
Diego Pollheim <diegopollheim@gmail.com>
2021-09-12 16:00:29 -0300

    Ex manipulando strings

--- Manipulando Strings/Manipulando Strings/Form1.cs
+++ Manipulando Strings/Manipulando Strings/Form1.cs
@@ -19,17 +19,26 @@ namespace Manipulando_Strings
 
         private void btnTam_Click(object sender, EventArgs e)
         {
-
+            txtResultado.Text = txtPalavra.Text.Length.ToString();
         }
 
         private void btnQtdPalavras_Click(object sender, EventArgs e)
         {
-
+            string frase = txtPalavra.Text;
+            string[] palavras = frase.Split(' ');
+            txtResultado.Text = palavras.Length.ToString();
         }
 
         private void btnInverter_Click(object sender, EventArgs e)
         {
+            string palavra = txtPalavra.Text;
+            string palavraInvertida = "";
 
+            for (int i = palavra.Length-1; i >= 0; i--)
+            {
+                palavraInvertida += palavra[i];
+            }
+            txtResultado.Text = palavraInvertida.ToString();
         }
     }
 }
